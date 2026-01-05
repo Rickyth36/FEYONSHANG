79316353854b76cadbef7e92c2faae6dfa79be5a
Ricky Thoudam <rickythoudam123@gmail.com>
2026-01-05 16:41:56 +0530

    backend started

---
+++ backend/models/productModel.js
@@ -0,0 +1,45 @@
+import { request } from "express";
+import mongoose from "mongoose";
+import BestSeller from "../../frontend/src/components/BestSeller";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: Array,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    subCategory: {
+        type: String,
+        required: true
+    },
+    sizes: {
+        type: Array,
+        required: true
+    },
+    BestSeller: {
+        type: Boolean
+    },
+    date: {
+        type: Number,
+        required: true
+    }
+})
+
+const productModel = mongoose.models.product || mongoose.model("product",productSchema);
+
+export default productModel;
