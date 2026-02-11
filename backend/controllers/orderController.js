7287aa57978e8f52bdba54323c27f4135d9201cc
Ricky Thoudam <rickythoudam123@gmail.com>
2026-02-11 22:48:20 +0530

    stripe payment updated

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,6 +2,11 @@ import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
 import Stripe from "stripe";
 
+
+// Global variables
+const currency = 'inr'
+const deliveryCharge = 10
+
 // gateway initialize
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
@@ -45,11 +50,61 @@ const placeOrderStripe = async (req, res) => {
     };    
     const newOrder = new orderModel(orderData);
     await newOrder.save();
+    const line_items = items.map((item) => ({
+      price_data: {
+        currency,
+        product_data: {
+          name: item.name
+        },
+        unit_amount: item.price * 100
+      },
+      quantity: item.quantity
+    }))
+    line_items.push({
+      price_data: {
+        currency,
+        product_data: {
+          name: "Delivery charges"
+        },
+        unit_amount: deliveryCharge * 100
+      },       
+      quantity: 1
+    })
+    const session = await stripe.checkout.sessions.create({
+      success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+      line_items,
+      mode: 'payment'
+    })
 
+    res.json({
+      success: true,
+      session_url: session.url,
+    })
+ 
   } catch (error) {
-    
+    console.log(error);
+    res.json({success: false, message: error.message})
   }
 };
+
+// Verify stripe
+const verifyStripe = async(req,res) => {
+  const {orderId, success, userId} = req.body;
+  try {
+    if(success === "true"){
+      await orderModel.findByIdAndUpdate(orderId, {payment: true});
+      await userModel.findByIdAndUpdate(userId, {cartData: {}})
+      res.json({success: true});
+    } else {
+      await orderModel.findByIdAndDelete(orderId)
+      res.json({success: false})
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({success: false, message: error.message}) 
+  }
+}
 
 // Razorpay method
 const placeOrderRazorpay = async (req, res) => {};
