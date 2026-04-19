183a85c80b1f7e3477f37b9692095bdcecaa0c6c
Ricky Thoudam <rickythoudam123@gmail.com>
2026-04-19 15:52:55 +0530

    config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,10 +14,12 @@ import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
 import Verify from './pages/Verify'
 import { ToastContainer } from 'react-toastify';
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]' >
+      <ScrollToTop />      
       <ToastContainer />
       <Navbar />
       <SearchBar />
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,38 +1,56 @@
-import React from 'react'
-import { assets } from '../assets/assets'
+import React, { useContext } from "react";
+import { assets } from "../assets/assets";
+import { ShopContext } from "../context/ShopContext";
 
 const Footer = () => {
+  const { navigate } = useContext(ShopContext);
   return (
-    <div className='' >
-        <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 text-sm">
-            <div className="">
-                <img className='mb-5 w-32' src={assets.logo} alt="" />
-                <p className='w-full md:w-2/3 text-gray-600' >FEYONSHANG is dedicated to bringing you modern fashion that blends style, comfort and quality.
-                    From everyday essentials to statement pieces, our collections are designed to help you express your individuality with confidence</p>
-            </div>
-            <div className="">
-                <p className='text-xl font-medium mb-5' >COMPANY</p>
-                <ul className='flex flex-col gap-1 text-gray-600'>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Delivery</li>
-                    <li>Privacy Policy</li>
-                </ul>
-            </div>
-            <div className="">
-                <p className='text-xl font-medium mb-5' >GET IN TOUCH</p>
-                <ul className="flex flex-col gap-1 text-gray-600">
-                    <li>+1234567890</li>
-                    <li>contact@feyonshang.com</li>
-                </ul>
-            </div>
-        </div>  
+    <div className="">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 text-sm">
         <div className="">
-            <hr />
-            <p className='py-5 text-sm text-center'>Copyright {new Date().getFullYear()}@feyonshang.com. All Rights Reserved</p>
+          <img className="mb-5 w-32" src={assets.logo} alt="" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            FEYONSHANG is dedicated to bringing you modern fashion that blends
+            style, comfort and quality. From everyday essentials to statement
+            pieces, our collections are designed to help you express your
+            individuality with confidence
+          </p>
         </div>
+        <div className="">
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li className="cursor-pointer" onClick={() => navigate("/")}>
+              Home
+            </li>
+            <li className="cursor-pointer" onClick={() => navigate("/about")}>
+              About us
+            </li>
+            <li className="cursor-pointer" onClick={() => navigate("/orders")}>
+              Delivery
+            </li>
+            <li className="cursor-pointer" onClick={() => navigate("/contact")}>
+              Contact
+            </li>
+            {/* <li>Privacy Policy</li> */}
+          </ul>
+        </div>
+        <div className="">
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+1234567890</li>
+            <li>contact@feyonshang.com</li>
+          </ul>
+        </div>
+      </div>
+      <div className="">
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright {new Date().getFullYear()}@feyonshang.com. All Rights
+          Reserved
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
